fbe7b2f755c48337946ca6e6963e8937e5d8642d
alexceron1 <alex_ceron1@outlook.com>
2024-01-06 00:05:11 -0600

    Se modifico clase ClaseCalendar

--- src/main/java/claseDateYCalendar/ClaseCalendar.java
+++ src/main/java/claseDateYCalendar/ClaseCalendar.java
@@ -2,12 +2,29 @@ package claseDateYCalendar;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 public class ClaseCalendar {
     public static void main(String[] args){
-        Calendar calendario = Calendar.getInstance();
-        calendario.set(2004,Calendar.SEPTEMBER, 19);
+        Calendar calendar = Calendar.getInstance();
+        /**
+         * Agregar una fecha
+         */
+        //calendar.set(2024, Calendar.JANUARY, 5, 23, 40, 59);
+        calendar.set(Calendar.YEAR, 2024);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 05);
         
-        Date fecha = calendario.getTime();
-        System.out.println("fecha = " + fecha);
+        //calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.HOUR, 11);
+        calendar.set(Calendar.AM_PM, Calendar.PM);
+        calendar.set(Calendar.MINUTE, 40);
+        calendar.set(Calendar.SECOND, 59);
+        
+        Date date = calendar.getTime();
+        System.out.println("Fecha sin formato = " + date);
+        
+        SimpleDateFormat sdFormat = new SimpleDateFormat("EEEE d 'de' MMMM, yyyy  hh:mm:ss a");
+        String dateWithFormat = sdFormat.format(date);
+        System.out.println("Fecha con formato = " + dateWithFormat);
     }
 }
